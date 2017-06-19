fcb0e6343361d890a1d57e3d62bef38a79c5c4f9
Eton <eton.williams@gmail.com>
2017-06-19 19:04:40 +0200

    add files

---
+++ chapter03 Objects Types and Values/chapter03_c++ code in solutions/chapter03_02_mile_to_kilometer_conversion.cpp
@@ -0,0 +1,18 @@
+// chapter03_02_mile_to_kilometer_conversion.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int miles, kilometers;
+	cout << "This program converts miles to kilometers.\n";
+	cout << "\nEnter a number of miles: ";
+	cin >> miles;
+	kilometers = miles*1.609;
+	cout << "\n" << miles << " miles equals " << kilometers << " kilometers. \n" ;
+	cout << "\nGood-bye \n\n";
+	return 0;
+}
---
+++ chapter03 Objects Types and Values/chapter03_c++ code in solutions/chapter03_04 small large sum diff ratio.cpp
@@ -0,0 +1,42 @@
+// chapter03_04 small large sum diff ratio.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	double val1,val2;
+	double smallest, largest, sum, difference1, difference2, product, ratio1, ratio2; 
+	cout << "Enter two integers: :";
+	cin >> val1 >> val2;
+	
+	if (val1 > val2) {
+			smallest = val2;
+			largest = val1;
+	}
+	else {
+			smallest = val1;
+			largest = val2;
+	}
+	
+	sum = val1+val2;  // why doesnt this work?
+	difference1 = val1 - val2;
+	difference2 = val2 - val1;
+	product = val1 * val2;
+	ratio1 = val1 / val2;
+	ratio2 = val2 / val1;
+
+	cout << "\nSmallest number is "  << smallest << " \n\n";
+	cout << "\nLargest  number is "  << largest << " \n\n";
+	cout << "\nSum is "				 << largest << " \n\n";
+	cout << "\nDifference1 is "		 << difference1 << " \n\n";
+	cout << "\nDifference2 is "		 << difference2 << " \n\n";
+	cout << "\nProduct is "			 << product << " \n\n";
+	cout << "\nRatio1 is "			 << ratio1 << " \n\n";
+	cout << "\nRatio2 is "			 << ratio2 << " \n\n";
+	
+	return 0;
+}
+
