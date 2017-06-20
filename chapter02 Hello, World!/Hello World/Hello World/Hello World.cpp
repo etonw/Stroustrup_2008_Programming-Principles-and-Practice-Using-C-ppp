e75a18a4455f75e34ed5619f5f32c86bc12d66ac
Eton <eton.williams@gmail.com>
2017-06-20 13:07:35 +0200

    added cin.get()

---
+++ chapter02 Hello, World!/Hello World/Hello World/Hello World.cpp
@@ -0,0 +1,27 @@
+/*
+	etonw
+	20.6.2017
+	PPP chapter02
+	Hello World.cpp 
+	Print "Hello World" to the screen.
+*/
+
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include<vector>
+#include<algorithm>
+#include<stdexcept>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World \n\n";
+
+	cin.get(); // wait for ENTER before closing window
+	return 0;
+}
