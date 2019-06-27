299e43da8d05c1db0c133017bdc954d39cd7387b
Eton Williams <eton.williams@gmail.com>
2019-06-27 12:52:03 +0200

    Add files via upload

---
+++ 2 Hello World/hello_programming.cpp
@@ -0,0 +1,18 @@
+// include at the beginning of every Stroustrup exercise
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
+
+int main()         // C++ programs start by executing the function main
+{
+         cout << "Hello, programming!\n";    // output “Hello, World!”
+         cout << "Here we go!\n";  
+         keep_window_open();           // wait for a character to be entered
+         return 0;
+}
